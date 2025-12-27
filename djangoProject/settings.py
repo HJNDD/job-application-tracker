007230846c67ddd31cc70c84b1898df231d86a99
bingfeng <lbf2571712686@gmail.com>
2025-12-27 18:58:38 +0800

    Add CORS support for React frontend

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,15 +39,17 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
-
+    "corsheaders",
     # third-party
     'rest_framework',
+
 
     'jobs',
     'accounts'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = "djangoProject.urls"
