326332464683b932748e286e26c1a2a4be06f74e
bingfeng <lbf2571712686@gmail.com>
2025-12-27 19:41:59 +0800

    refactor: remove legacy root backend files after moving to backend/

--- jobs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Job
-
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    list_display = ("id", "company", "title", "status", "owner", "applied_at", "updated_at")
-    list_filter = ("status",)
-    search_fields = ("company", "title")
